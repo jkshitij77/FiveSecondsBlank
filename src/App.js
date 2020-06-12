21bfa9a657183e3ad6ed8bfb0728ae0ad384f8bf
Kshitij Jain <kshitij.jain.23@dartmouth.edu>
2020-06-13 00:12:23 +0530

    Added the Get quote button and about us section )tentative)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import './App.css';
 
+function gimmethequote(){
+  console.log("getting the quote");
+}
+
 function App() {
   return (
     <div class="body">
+
+      <div class="floatingbutton" onClick={gimmethequote}>Get quote</div>
+
+
       {/* Header part is here
           We have our banner with the text and logo here */}
       <div class="header">
@@ -12,8 +20,8 @@ function App() {
             <img src={require("./ghoulc.jpg")} alt="Logo" class="logo"></img>
           </div>
           <div class="textInsideBanner">
-            <h1>Five seconds blank</h1>
-            <h3>Blend my shit</h3>
+            <h1>Five Seconds Blank</h1>
+            <h3>Feel Scenarios Blend</h3>
           </div>
         </div>
       </div>
@@ -21,7 +29,11 @@ function App() {
 
       {/* We have the about us section here */}
       <div class="aboutus">
-        
+        <h3>ABOUT US</h3>
+        <p>Lorem ipsum dolor Lorem ipsum dolorLorem ipsum dolorLorem ipsum dolorLorem ipsum dolorLorem ipsum dolorLorem ipsum dolor</p>
+        <p>Lorem ipsum dolor Lorem ipsum dolorLorem ipsum dolorLorem ipsum dolorLorem ipsum dolorLorem ipsum dolorLorem ipsum dolor</p>
+        <p>Lorem ipsum dolor Lorem ipsum dolorLorem ipsum dolorLorem ipsum dolorLorem ipsum dolorLorem ipsum dolorLorem ipsum dolor</p>
+          
       </div>
     </div>
   );
