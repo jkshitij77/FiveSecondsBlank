2bb21dfddf52b2c878c3d67ab9321ba1f1cb6fc0
Kshitij Jain <kshitij.jain.23@dartmouth.edu>
2020-06-12 23:52:16 +0530

    Banner ready

--- src/App.js
+++ src/App.js
@@ -4,11 +4,24 @@ import './App.css';
 function App() {
   return (
     <div class="body">
+      {/* Header part is here
+          We have our banner with the text and logo here */}
       <div class="header">
         <div class="insideBanner">
-          <h1>Five seconds blank</h1>
-          <h3>Bled my shit</h3>
+          <div class="pictureInsideBanner">
+            <img src={require("./ghoulc.jpg")} alt="Logo" class="logo"></img>
+          </div>
+          <div class="textInsideBanner">
+            <h1>Five seconds blank</h1>
+            <h3>Blend my shit</h3>
+          </div>
         </div>
+      </div>
+
+
+      {/* We have the about us section here */}
+      <div class="aboutus">
+        
       </div>
     </div>
   );
