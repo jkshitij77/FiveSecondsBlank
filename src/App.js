f9db873c4713bfc568925041c248244f87acbaa1
Kshitij Jain <kshitij.jain.23@dartmouth.edu>
2020-06-12 23:29:14 +0530

    New stuff added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="body">
+      <div class="header">
+        <div class="insideBanner">
+          <h1>Five seconds blank</h1>
+          <h3>Bled my shit</h3>
+        </div>
+      </div>
     </div>
   );
 }
