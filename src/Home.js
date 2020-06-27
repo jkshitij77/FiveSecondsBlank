bb3686565f182798a61f154eccec258f5075375f
Your Name <you@example.com>
2020-06-27 12:36:44 +0530

    the pull before my push - shuchit

--- src/Home.js
+++ src/Home.js
@@ -8,7 +8,7 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
-import { TiPhone } from "react-icons/ti";
+
 import Collapsible from 'react-collapsible';
 
 class Home extends Component{
